ee513b52a24daaed9f361215d759af2321e37596
Your Name <you@example.com>
2018-07-28 15:40:31 +0800

    12112121

---
+++ theme/terry/theme01/layouts/login.php
@@ -0,0 +1,41 @@
+<?php
+$jsOptions = [ # js的配置部分
+    [
+        # js options ，来定义位置，条件等
+        # 在当前options下的js文件
+        'js' => [
+
+        ],
+    ]
+
+];
+$cssOptions = [
+    # css配置
+    [
+        'css'	=>[
+
+        ],
+    ],
+];
+\Yii::$service->page->asset->jsOptions = $jsOptions;
+\Yii::$service->page->asset->cssOptions = $cssOptions;
+\Yii::$service->page->asset->register($this);
+
+
+?>
+
+<?php $this->beginPage() ?>
+  <!doctype html>
+  <html lang="en">
+  <head>
+    <title>晋彤后台管理系统-登录页面</title>
+      <?php $this->beginBody() ?>
+      <?php $this->endBody() ?>
+    <meta charset="UTF-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <?= $content ?>
+  </body>
+  </html>
+<?php $this->endPage() ?>
