a10028a5faa5c85dc279e060238b354e737daf3a
tomorrowzjz <1047547064@qq.com>
2018-08-04 11:45:51 +0800

    'zanwushuju'

--- local/local_modules/Water/controllers/AccountController.php
+++ local/local_modules/Water/controllers/AccountController.php
@@ -83,18 +83,21 @@ class AccountController extends PublicsController
     // 实名认证
 
     public function actionRealname(){
-        $_SESSION['pagess']='realname';
+        $res = Yii::$app->db->createCommand("select * from shop where uid='{$_SESSION[uid]}'")->queryOne();
 
-        return $this->render($this->action->id);
+        $datas["res"] = $res;
+        $_SESSION['pagess']='realname';
+        return $this->render($this->action->id,$datas);
     }
 
 //=================================资金列表=================================
     // 资金列表
 
     public function actionMoney(){
+        $data = [];
         $_SESSION['pagess']='money';
 
-        return $this->render($this->action->id);
+        return $this->render($this->action->id,$data);
     }
 
 
@@ -103,9 +106,91 @@ class AccountController extends PublicsController
     // 账户解冻
 
     public function actionThawing(){
+
+        $datas = [];
+        $shop_state = Yii::$app->db->createCommand("select shop_state from shop where shop_id={$_SESSION["shop_id"]}")->queryOne();
+        $shop_state = $shop_state["shop_state"];
+        if($shop_state == 2){
+
+            $res = Yii::$app->db->createCommand("select thaw_info.*,shop.freezing_time,shop.freezing_cause from thaw_info,shop where thaw_info.uid=shop.uid and shop.uid={$_SESSION["uid"]} order by id desc")->queryOne();
+
+            if($res["status"]!=1){
+
+                $datas["res"] = $res;
+            }else{
+                $res = [];
+                $res["status"] = 1;
+                $datas["res"] = $res;
+            }
+
+        }
+        $datas["shop_state"] = $shop_state;
         $_SESSION['pagess']='thawing';
 
-        return $this->render($this->action->id);
+
+        return $this->render($this->action->id,$datas);
+    }
+
+    //账户解冻申请
+    public function actionApply(){
+
+        $req = Yii::$app->request;
+
+        $post = $req->post();
+
+        $folder ='../../appimage/common/images/';
+
+        $file=$_FILES['voucher'];
+
+        $n = count($file["name"]);
+
+        $img = [];
+
+        for ($i = 0;$i<$n;$i++) {
+
+            //接受文件名
+            $name = $file["name"][$i];
+
+            //接受临时目录
+            $tmp_name = $file["tmp_name"][$i];
+
+            //接受错误编码
+            $error = $file["error"][$i];
+
+            if ($error == 0) {
+                // 检测文件是否来自于表单
+                if (is_uploaded_file($tmp_name)) {
+                    # code...
+                    // 新的文件名
+                    $ext = substr($name, strrpos($name, '.'));
+
+                    $newName = time() . rand() . $ext;
+
+                    // 进行上传
+
+                    if (move_uploaded_file($tmp_name, $folder . $newName)) {
+                        $img[] = "/images/" . $newName;
+
+
+                    }
+                }
+            }
+
+        }
+        if($post["status"] == 2){
+            $time = time();
+            $voucher = $post["voucher"]."||".implode("||",$img);
+            $sql = "update thaw_info set `desc`='{$post["desc"]}',voucher='$voucher',create_date='{$time}',status=0 where uid={$_SESSION["uid"]}";
+        }else{
+            $time = time();
+            $voucher = implode("||",$img);
+            $sql = "insert into thaw_info (`desc`,voucher,create_date,uid,status) values ('{$post["desc"]}','$voucher','{$time}','{$_SESSION['uid']}',0)";
+
+        }
+        $res = Yii::$app->db->createCommand($sql)->execute();
+
+
+        return $this->redirect("/shop/account/thawing");
     }
     
 }
